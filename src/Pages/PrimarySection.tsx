1f5897096c7e8388549417f4f9a0dbf88ba44aee
Yasser-Slaymen <yaserslyman0@gmail.com>
2023-01-18 02:10:49 +0100

    primarmain finshed

--- src/Pages/PrimarySection.tsx
+++ src/Pages/PrimarySection.tsx
@@ -6,20 +6,21 @@ import PrimSecond from '../lib/Components/PrimSecond';
 function PrimarySection (){
     // Fetch data from custom hook that uses React-Query
     return(
-        <div className="primarymain">
-            <Title> 
-                <div className="primarySction">
-                    <h1 className='primTitle'><span>Business Solution <br />by Digital</span></h1>
-                    <h1 className='primAgency'>Agency</h1>
-                    <p className="primText">
+    <div className="primarymain">
+        <Title> 
+            <div className="primarySction">
+                <h1 className='primTitle'><span>Business Solution <br />by Digital</span></h1>
+                <h2 className='primAgency'>Agency</h2>
+                <p className="primText">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
-                    nostrum dolorem vel, deserunt assumenda harum necessitatibus nemo, 
-                    </p>
-                </div>
-            </Title>
-            <PrimSecond/>
+                    nostrum dolorem vel, deserunt assumenda harum necessitat<br /> 
+                    nostrum dolorem vel, deserunt assumenda harum necessitat
+                </p>
+            </div>
+        </Title>
+        <PrimSecond/>
 
-        </div> 
+    </div> 
     );
 }
 export default PrimarySection;
