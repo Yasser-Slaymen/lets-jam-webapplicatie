4d4afd0f4d8e36c5520302b955c10527566fbc2d
Yasser-Slaymen <yaserslyman0@gmail.com>
2023-01-27 13:59:17 +0100

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// import {Link, Route, Routes} from 'react-router-dom'
 import "./App.css";
 import ThirdSection from "./Pages/ThirdSection";
 import HeadSect from "./Pages/HeadSect";
@@ -6,12 +5,10 @@ import PrimarySection from "./Pages/PrimarySection";
 import SecondSection from "./Pages/SecondSection";
 import FouthSection from "./Pages/FourthSection";
 import FooterSection from "./Pages/FooterSection";
-// import Jst from "./lib/Components/Jstn";
 
 function App() {
   return (
     <div className="App">
-      {/* <Jst /> */}
       <HeadSect />
       <PrimarySection />
       <SecondSection />
